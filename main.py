4681ef56c820a763b88b7bdee4c6734651a9765e
徐英杰 <806895158@qq.com>
2025-04-01 19:46:48 +0800

    修改文档描述

--- main.py
+++ main.py
@@ -7,12 +7,11 @@ from astrbot.core.star.filter.event_message_type import EventMessageType
 from data.plugins.astrbot_plugin_dy.api_collection import video
 
 
-@register("helloworld", "YourName", "一个简单的 Hello World 插件", "1.0.0")
+@register("抖音解析", "黄四郎", "抖音解析", "1.1.1")
 class MyPlugin(Star):
     def __init__(self, context: Context):
         super().__init__(context)
-    
-    # 注册指令的装饰器。指令名为 helloworld。注册成功后，发送 `/helloworld` 就会触发这个指令，并回复 `你好, {user_name}!`
+
     @filter.event_message_type(EventMessageType.ALL)
     async def helloworld(self, event: AstrMessageEvent):
         message_str = event.message_str # 用户发的纯文本消息字符串
